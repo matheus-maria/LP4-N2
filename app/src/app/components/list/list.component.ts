9d516b748dc499429a00db17e43d2c0098beb920
matheus-maria <mmatheus.maria@gmail.com>
2019-09-17 22:10:43 -0300

    Table

--- app/src/app/components/list/list.component.ts
+++ app/src/app/components/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from 'src/app/api/common.service';
 import { Clothes } from 'src/app/api/common.models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +10,21 @@ import { Clothes } from 'src/app/api/common.models';
 })
 export class ListComponent implements OnInit {
 
-  constructor(private api: CommonService) { }
+  constructor(private api: CommonService, private router: Router) { }
 
   dataList: Clothes[]
+  displayedColumns: string[] = ['code', 'features', 'entryAt', 'price', 'actions'];  
 
   async ngOnInit() {
-    this.dataList = await this.api.getData()
+    this.dataList = await this.api.getData()    
+  }
+
+  remove = (id: number) => {
+    console.log("Id",id)
+  }
+
+  edit = (id: number) => {
+    this.router.navigate(['/details', id])
   }
 
 }
